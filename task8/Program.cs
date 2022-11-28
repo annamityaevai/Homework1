3f317c3b2495052781320d39610da6e05fa3d62a
Anna Mityaeva <aiisa@inbox.ru>
2022-11-28 09:41:24 +0300

    added task 8

---
+++ task8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Insert number");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+
+while(a <= N)
+{
+    if(a%2 == 0){
+        Console.WriteLine(a);
+        a = a + 1;
+    }
+    else{
+        a = a + 1;
+    }
+}
