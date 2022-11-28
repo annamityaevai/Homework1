4c61db6a3fecbb37a6db2a838aff7c1e3042c251
Anna Mityaeva <aiisa@inbox.ru>
2022-11-28 09:16:35 +0300

    added task 2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Input number a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number b");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    Console.WriteLine("max " + a);
+    Console.WriteLine("min " + b);
+}
+else
+{
+    Console.WriteLine("max " + b);
+    Console.WriteLine("min " + a);
+}
